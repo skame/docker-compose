45ac6945dc0fc5280ec1d61290b5d0a8ed6adda6
skame <skame@nttv6.jp>
2017-01-21 15:30:20 +0900

    add docker-entrypoint.sh and set DOCKER_HOST

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		--storage-driver=vfs \
+		"$@"
+fi
+
+if [ "$1" = 'dockerd' ]; then
+	# if we're running Docker, let's pipe through dind
+	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
+	set -- sh "$(which dind)" "$@"
+fi
+
+# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
+if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
+	export DOCKER_HOST='tcp://docker:2375'
+fi
+
+exec "$@"
